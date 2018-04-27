662c22c17995e7210b41a5de084f3808d063a146
Yusuke Sangenya <longinus.eva@gmail.com>
2018-04-27 14:49:27 +0900

    Create web api.

--- main.rb
+++ main.rb
@@ -1,10 +1,21 @@
 $LOAD_PATH.push('./lib')
 
 require 'kumano_tasks'
+require 'sinatra'
 
 API_KEY=ENV['API_KEY'].freeze
 REPOSITORY=ENV['REPOSITORY'].freeze
+USERNAME=ENV['USERNAME'].freeze
+PASSWORD=ENV['PASSWORD'].freeze
 
-reader = KumanoTasks::GithubReader.new(API_KEY, REPOSITORY)
-writer = KumanoTasks::GithubWriter.new(API_KEY, REPOSITORY)
-puts writer.create_issue(reader.issues_grouped_by_label)
+use Rack::Auth::Basic do |username, password|
+  username == USERNAME && password == PASSWORD
+end
+
+get '/create_issue' do
+  reader = KumanoTasks::GithubReader.new(API_KEY, REPOSITORY)
+  writer = KumanoTasks::GithubWriter.new(API_KEY, REPOSITORY)
+  new_issue = writer.create_issue(reader.issues_grouped_by_label)
+
+  redirect new_issue.html_url
+end
