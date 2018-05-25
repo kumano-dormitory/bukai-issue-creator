b06daa33779b792be5ae3bc9e375a58fbab06dba
Yusuke Sangenya <longinus.eva@gmail.com>
2018-05-25 21:52:18 +0900

    Read with graphql.
    In order to filter CrossReferencedEvent.

--- lib/kumano_tasks/github_reader.rb
+++ lib/kumano_tasks/github_reader.rb
@@ -1,24 +1,95 @@
-require 'octokit'
 require 'dotenv/load'
+require "graphql/client"
+require "graphql/client/http"
+require 'date'
+require 'time'
 
 module KumanoTasks
+  module GithubGraphQL
+    HTTP = GraphQL::Client::HTTP.new("https://api.github.com/graphql") do
+      def headers(context)
+        { "Authorization" => "bearer #{ENV['API_KEY']}" }
+      end
+    end
+    Schema = GraphQL::Client.load_schema(HTTP)
+    Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+  end
+
   class GithubReader
-    def initialize(api_key, repository_name)
-      @api_key = api_key
-      @repository_name = repository_name
-      @client = Octokit::Client.new(access_token: @api_key)
+    def self.since
+      (Date.today - 7).to_time # 7 days ago
     end
 
-    def client
-      @client
+    def self.timeline_event_queries
+      event_types = GithubGraphQL::Schema.as_json['data']['__schema']['types']
+                                         .find { |t| t['name'] == 'IssueTimelineItem' }['possibleTypes']
+                                         .select { |t| t['name'] != 'Commit' } .map { |t| t['name'] }
+
+      event_types.map { |t| "... on #{t} { createdAt }" }.join(",\n")
     end
 
-    def issues(since: (Date.today - 7).to_time) # 7 days ago
-      @issues ||= open_issues + closed_updated_issues(since)
+    OpenIssues = GithubGraphQL::Client.parse <<-QUERY
+      query {
+        repository(owner: "#{ENV['OWNER']}", name: "#{ENV['REPOSITORY_NAME']}") { 
+          issues(last: 100, states: [OPEN]) {
+            nodes {
+              title,
+              url,
+              labels(first: 10) {
+                nodes {
+                  id,
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    QUERY
+
+    ClosedIssues = GithubGraphQL::Client.parse <<-QUERY
+      query {
+        repository(owner: "#{ENV['OWNER']}", name: "#{ENV['REPOSITORY_NAME']}") { 
+          issues(first: 100, states: [CLOSED], orderBy: { direction: DESC, field: UPDATED_AT }) {
+            nodes {
+              title,
+              url,
+              labels(first: 10) {
+                nodes {
+                  id,
+                  name
+                }
+              },
+              timeline(last: 100, since: "#{self.since.iso8601}") {
+                nodes {
+                  #{self.timeline_event_queries}
+                }
+              }
+            }
+          }
+        }
+      }
+    QUERY
+
+    Labels = GithubGraphQL::Client.parse <<-QUERY
+      query {
+        repository(owner: "#{ENV['OWNER']}", name: "#{ENV['REPOSITORY_NAME']}") {
+          labels(first: 100) {
+            nodes {
+              id,
+              name
+            }
+          }
+        }
+      }
+    QUERY
+
+    def issues
+      @issues ||= open_issues + closed_updated_issues
     end
 
     def section_labels
-      @section_labels ||= client.labels(@repository_name).select do |label|
+      @section_labels ||= GithubGraphQL::Client.query(Labels).data.repository.labels.nodes.select do |label|
         label.name.match(/セクション/)
       end
     end
@@ -28,7 +99,7 @@ module KumanoTasks
         [name, []]
       end.to_h
       issues.each do |issue|
-        label = section_labels.find { |section_label| issue.labels.any? { |issue_label| issue_label.id == section_label.id } }
+        label = section_labels.find { |section_label| issue.labels.nodes.any? { |issue_label| issue_label.id == section_label.id } }
         if label.nil?
           grouped_issues['その他'].push(issue)
         else
@@ -40,13 +111,24 @@ module KumanoTasks
 
     private
     def open_issues
-      client.issues(@repository_name)
+      GithubGraphQL::Client.query(OpenIssues).data.repository.issues.nodes
     end
 
-    def closed_updated_issues(since)
-      client.issues(@repository_name, state: :closed).select { |issue|
-        issue.updated_at > since
-      }
+    def closed_updated_issues
+      GithubGraphQL::Client.query(ClosedIssues).data.repository.issues.nodes.select do |issue|
+        events_other_than_referenced = issue.timeline.nodes.select do |event|
+          event.created_at? && Time.iso8601(event.created_at) > self.class.since
+        end.select do |event|
+          event.__typename != 'CrossReferencedEvent'
+        end
+
+        !events_other_than_referenced.empty?
+      end
     end
   end
+end
+
+if __FILE__ == $0
+  reader = KumanoTasks::GithubReader.new
+  puts reader.issues_grouped_by_label
 end
